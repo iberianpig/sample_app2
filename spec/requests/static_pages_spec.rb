03fd1d0ac4776c8ce98484428176d1b1338ee8d8
iberianpig <yhkyky@gmail.com>
2014-05-31 18:16:47 +0900

    こみっとだよー これはrspecのファイルを追加したんだよ

---
+++ spec/requests/static_pages_spec.rb
@@ -0,0 +1,13 @@
+require 'spec_helper'
+
+describe "Static pages" do
+  describe "Home page" do
+    it "should have the content 'Sample App'" do
+      # Run the generator again with the --webrat flag if you want to use webrat methods/matchers
+      # get static_pages_index_path
+      visit '/static_pages/home'
+      # response.status.should be(200)
+      expect(page).to have_content('Sample App')
+    end
+  end
+end
