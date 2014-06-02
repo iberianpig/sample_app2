509f4bee0948fe902221225d7e68a72fe17cfe0b
iberianpig <yhkyky@gmail.com>
2014-06-03 08:59:13 +0900

    2014/06/02 3-2-1冒頭まで(pry入れてみた)

--- Gemfile
+++ Gemfile
@@ -7,6 +7,8 @@ gem 'rails', '4.0.2'
 group :development, :test do
   gem 'sqlite3', '1.3.8'
   gem 'rspec-rails', '2.13.1'
+  # gem 'debugger',
+  gem 'pry-rails'
 end
 
 group :test do
--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -1,4 +1,5 @@
 require 'spec_helper'
+require 'pry'
 
 describe "Static pages" do
   describe "Home page" do
@@ -8,6 +9,7 @@ describe "Static pages" do
       visit '/static_pages/home'
       # response.status.should be(200)
       expect(page).to have_content('Sample App')
+      # binding.pry
     end
   end
 end
